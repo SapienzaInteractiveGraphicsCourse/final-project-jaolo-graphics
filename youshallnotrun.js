cbc440e4053d12c00a25e3b8442fcac3e12b7f01
giancarlo-campeti <82371037+giancarlo-campeti@users.noreply.github.com>
2021-09-26 11:54:59 +0200

    Update youshallnotrun.js

--- youshallnotrun.js
+++ youshallnotrun.js
@@ -260,7 +260,7 @@ function init() {
 
 
   // background Texture
-  var texture = texture_loader.load("/res/bg/bgtry.jpg")
+  var texture = texture_loader.load("final-project-jaolo-graphics/res/bg/bgtry.jpg")
   texture.encoding = THREE.sRGBEncoding;
   texture.anisotropy = 16;
   texture.wrapS = THREE.RepeatWrapping;
